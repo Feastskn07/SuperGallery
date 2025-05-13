73a331256412a059a66faa0f20dbea0cc0d4f0a4
ibrahim taskin <ibrahimtaskin3807@gmail.com>
2025-05-13 05:25:27 +0300

    Proje oluşturuldu.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace supersportgallert.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
