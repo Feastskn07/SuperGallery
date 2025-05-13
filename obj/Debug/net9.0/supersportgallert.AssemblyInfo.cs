6b2bd99f994487289ad3621dba1356a012c70229
ibrahim taskin <ibrahimtaskin3807@gmail.com>
2025-05-13 05:32:04 +0300

    Git Ignore oluşturuldu..

--- obj/Debug/net9.0/supersportgallert.AssemblyInfo.cs
+++ obj/Debug/net9.0/supersportgallert.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("supersportgallert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a331256412a059a66faa0f20dbea0cc0d4f0a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("supersportgallert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("supersportgallert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
